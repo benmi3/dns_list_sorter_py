8577a97af5527eef693fad5e8c13298c86aee6e1
Benjamin Jørgensen <me@benmi.me>
2024-01-08 17:16:04 +0900

    should be closing in to finished now

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import toml
-import request
+import requests
+from time import sleep
 
-def iterate_toml_file(file_path) -> dict:
+def get_toml_data(file_path) -> dict:
     try:
         # Load TOML file
         with open(file_path, 'r') as file:
             toml_data = toml.load(file)
 
-        # Iterate through the TOML data
-        for section, values in toml_data.items():
-            print(f"Section: {section}")
-            for key, value in values.items():
-                print(f"  {key}: {value}")
+            return toml_data
 
     except FileNotFoundError:
         print(f"Error: File '{file_path}' not found.")
@@ -19,30 +16,44 @@ def iterate_toml_file(file_path) -> dict:
         print(f"Error: {e}")
 
 
+def file_write_filter(cur_section: str, cur_line: str) -> bool:
+    if not cur_line.startswith("#"):
+        print(f"section: {cur_section} \nLine: {cur_line}")
+        list_name = f"sorted_list_{cur_section}.text"
+        with open(list_name) as file_print:
+            file_print.write(f"\n{cur_line}")
+        return True
+    else:
+        return False
 
 
 def main():
+
     path_to_file = "raw_lists.toml"
     toml_data = get_toml_data(path_to_file)
+    white_list = ['google.com',]
+
+    def filter_condition(item):
+        return not (isinstance(item, str) and (item.startswith("#") or any(word in item for word in white_list)))
 
     # Iterate through the TOML data
     for section, values in toml_data.items():
         print(f"Section: {section}")
-        list_for_sorting = []
-        for key, value in values.items():
-            
+        list_name = f"sorted_list_{section}.txt"
+        section_list = []
+        with open(list_name, 'w+') as file:
+            for key, value in values.items():
+                r = requests.get(value)
+                if r.status_code == 200:
+                    print(r.status_code)
+                    new_list = r.text.split('\n')
+                    section_list = list(set(section_list).union(set(new_list)))
+                    section_list = list(filter(filter_condition, section_list + new_list))
+                else:
+                    print(f"Failed to retrieve content. Status code: {r.status_code}")
+            file.writelines(map(lambda x: x + '\n', section_list))
 
-        for key, value in values.items():
-            print(f"  {key}: {value}")
 
 
-
-# list of names
-names = ['Jessa', 'Eric', 'Bob']
-
-# open file in write mode
-with open(r'/sales.txt', 'w') as fp:
-    for item in names:
-        # write each item on a new line
-        fp.write("%s\n" % item)
-    print('Done')
+if __name__ == "__main__":
+    main()
